a1101151b9c454f9e593e917ee72e53a6f83ddce
Nozistance <nozistance@gmail.com>
2024-01-29 15:23:07 +0300

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
-    id("java")
+    java
+    idea
+    id("org.springframework.boot") version "3.2.2"
+    id("io.spring.dependency-management") version "1.1.4"
 }
 
 group = "io.nozistance"
@@ -10,8 +13,21 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.jsoup:jsoup:${property("jsoup-version")}")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.telegram:telegrambots-spring-boot-starter:${property("telegrambots-version")}")
+    implementation("javax.xml.bind:jaxb-api:${property("jaxb-api-version")}")
+    implementation("org.springframework.retry:spring-retry")
+    runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+}
+
+idea.module {
+    isDownloadJavadoc = true
+    isDownloadSources = true
 }
 
 tasks.test {
