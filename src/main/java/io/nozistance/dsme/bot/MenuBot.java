396c7d0d24460c8731a21efdb46d87ca36bd4494
Nozistance <nozistance@gmail.com>
2024-02-01 18:50:58 +0300

    Add MenuBotController, MenuBot (Echo mode) + Aspect

---
+++ src/main/java/io/nozistance/dsme/bot/MenuBot.java
@@ -0,0 +1,52 @@
+package io.nozistance.dsme.bot;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.starter.SpringWebhookBot;
+
+@Slf4j
+@Component
+public class MenuBot extends SpringWebhookBot {
+
+    private final BotProperties botProperties;
+    private final SetWebhook setWebhook;
+
+    public MenuBot(SetWebhook setWebhook, BotProperties botProperties) {
+        super(setWebhook, botProperties.getToken());
+        this.botProperties = botProperties;
+        this.setWebhook = setWebhook;
+    }
+
+    @PostConstruct
+    public void init() throws TelegramApiException {
+        setWebhook(setWebhook);
+    }
+
+    @Override
+    public String getBotPath() {
+        return "/update";
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botProperties.getName();
+    }
+
+    @Override
+    public BotApiMethod<?> onWebhookUpdateReceived(Update update) {
+        if (update.hasMessage() || update.getMessage().hasText()) {
+            Long chatId = update.getMessage().getChatId();
+            String text = update.getMessage().getText();
+            return SendMessage.builder()
+                    .chatId(chatId)
+                    .text(text)
+                    .build();
+        } else return null;
+    }
+}
