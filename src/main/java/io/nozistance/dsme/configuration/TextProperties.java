58fb7aa53c25465492010e8e20f6fbc64456ccfd
Nozistance <nozistance@gmail.com>
2024-02-05 13:10:46 +0300

    Add TextProperties

---
+++ src/main/java/io/nozistance/dsme/configuration/TextProperties.java
@@ -0,0 +1,12 @@
+package io.nozistance.dsme.configuration;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@AllArgsConstructor
+@ConfigurationProperties("text")
+public class TextProperties {
+
+}
