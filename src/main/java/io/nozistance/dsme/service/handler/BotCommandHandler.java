d1310f682e89eb1639cf8ffcb69d980c2a9253c9
Nozistance <nozistance@gmail.com>
2024-02-05 13:17:53 +0300

    Add basic command handling

--- src/main/java/io/nozistance/dsme/service/handler/BotCommandHandler.java
+++ src/main/java/io/nozistance/dsme/service/handler/BotCommandHandler.java
@@ -1,6 +1,5 @@
 package io.nozistance.dsme.service.handler;
 
-import io.nozistance.dsme.service.Handler;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.PartialBotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Message;
@@ -10,6 +9,28 @@ public class BotCommandHandler implements Handler<Message> {
 
     @Override
     public PartialBotApiMethod<?> handle(Message botCommand) {
+        return switch (botCommand.getText()) {
+            case "/day" -> day(botCommand);
+            case "/menu" -> menu(botCommand);
+            case "/subscribe" -> subscribe(botCommand);
+            case "/unsubscribe" -> unsubscribe(botCommand);
+            default -> null;
+        };
+    }
+
+    private PartialBotApiMethod<?> day(Message botCommand) {
+        return null;
+    }
+
+    private PartialBotApiMethod<?> menu(Message botCommand) {
+        return null;
+    }
+
+    private PartialBotApiMethod<?> subscribe(Message botCommand) {
+        return null;
+    }
+
+    private PartialBotApiMethod<?> unsubscribe(Message botCommand) {
         return null;
     }
 }
