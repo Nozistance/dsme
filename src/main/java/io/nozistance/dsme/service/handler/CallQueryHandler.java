8a9eb987ff0fb5b9444546f3e797cc11e9855283
Nozistance <nozistance@gmail.com>
2024-02-06 12:40:52 +0300

    Add basic callback queries handling

--- src/main/java/io/nozistance/dsme/service/handler/CallQueryHandler.java
+++ src/main/java/io/nozistance/dsme/service/handler/CallQueryHandler.java
@@ -1,6 +1,6 @@
 package io.nozistance.dsme.service.handler;
 
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.PartialBotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 
@@ -9,6 +9,18 @@ public class CallQueryHandler implements Handler<CallbackQuery> {
 
     @Override
     public PartialBotApiMethod<?> handle(CallbackQuery callbackQuery) {
+        if (callbackQuery.getData().startsWith("details:")) {
+            return details(callbackQuery);
+        } else if (callbackQuery.getData().startsWith("day:")) {
+            return day(callbackQuery);
+        } else return null;
+    }
+
+    private PartialBotApiMethod<?> details(CallbackQuery callbackQuery) {
+        return null;
+    }
+
+    private PartialBotApiMethod<?> day(CallbackQuery callbackQuery) {
         return null;
     }
 }
