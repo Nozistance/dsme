a8a0aa66fd0d355d0d489d3e425ecb7420af9626
Nozistance <nozistance@gmail.com>
2024-02-17 15:09:11 +0300

    Refactor AnswerTextService to decrease number of allocations

--- src/main/java/io/nozistance/dsme/service/AnswerTextService.java
+++ src/main/java/io/nozistance/dsme/service/AnswerTextService.java
@@ -3,7 +3,6 @@ package io.nozistance.dsme.service;
 import lombok.RequiredArgsConstructor;
 
 import java.util.Map;
-import java.util.Objects;
 
 @RequiredArgsConstructor
 public class AnswerTextService {
@@ -11,9 +10,9 @@ public class AnswerTextService {
     private final Map<String, String> answers;
 
     public String getAnswer(String key, Object... args) {
-        return answers.get(key)
-                .formatted(args == null
-                        ? new Objects[]{}
-                        : args);
+        return args == null
+                ? answers.get(key)
+                : answers.get(key)
+                .formatted(args);
     }
 }
