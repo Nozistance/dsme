a3ef4cc4ce39695851dfaa8c470fdeb3c4587780
Nozistance <nozistance@gmail.com>
2024-02-18 13:55:16 +0300

    Remove unused annotation @Slf4j

--- src/main/java/io/nozistance/dsme/service/DataFetchingService.java
+++ src/main/java/io/nozistance/dsme/service/DataFetchingService.java
@@ -1,18 +1,17 @@
 package io.nozistance.dsme.service;
 
-import lombok.SneakyThrows;
-import lombok.extern.slf4j.Slf4j;
+import java.io.IOException;
+import java.net.SocketTimeoutException;
+import java.net.URI;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.net.SocketTimeoutException;
-import java.net.URI;
+import lombok.SneakyThrows;
 
-@Slf4j
 @Service
 @EnableRetry
 public class DataFetchingService {
--- src/main/java/io/nozistance/dsme/telegram/Webhook.java
+++ src/main/java/io/nozistance/dsme/telegram/Webhook.java
@@ -1,17 +1,16 @@
 package io.nozistance.dsme.telegram;
 
+ import org.telegram.telegrambots.bots.TelegramWebhookBot;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
 import jakarta.annotation.PostConstruct;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.ToString;
-import lombok.extern.slf4j.Slf4j;
-import org.telegram.telegrambots.bots.TelegramWebhookBot;
-import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
-import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
-import org.telegram.telegrambots.meta.api.objects.Update;
 
-@Slf4j
 @Getter
 @ToString
 @RequiredArgsConstructor
