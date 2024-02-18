79207b1fd32aa6544bef6c4c29b218e966c12181
Nozistance <nozistance@gmail.com>
2024-02-18 13:56:38 +0300

    Fix exception handling in UpdateHandlerAspect

--- src/main/java/io/nozistance/dsme/aspect/UpdateHandlerAspect.java
+++ src/main/java/io/nozistance/dsme/aspect/UpdateHandlerAspect.java
@@ -1,20 +1,34 @@
 package io.nozistance.dsme.aspect;
 
+import io.nozistance.dsme.service.AnswerTextService;
+import io.nozistance.dsme.telegram.UpdateAnswer;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
-
-import java.lang.reflect.UndeclaredThrowableException;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.bots.AbsSender;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Slf4j
 @Aspect
 @Component
+@RequiredArgsConstructor
 public class UpdateHandlerAspect {
 
-    @AfterThrowing(pointcut = "execution(* io.nozistance.dsme.telegram.UpdateHandler+.handle(..))", throwing = "e")
-    public void init(UndeclaredThrowableException e) {
-        log.error(e.getUndeclaredThrowable().getMessage(),
-                e.getUndeclaredThrowable());
+    private final AnswerTextService answers;
+
+    @Around("execution(* io.nozistance.dsme.telegram.UpdateHandler+.handle(..))")
+    public void init(ProceedingJoinPoint joinPoint) throws TelegramApiException {
+        Update update = (Update) joinPoint.getArgs()[0];
+        AbsSender sender = (AbsSender) joinPoint.getArgs()[1];
+        try {
+            joinPoint.proceed();
+        } catch (Throwable e) {
+            sender.execute(new UpdateAnswer(update, answers.getAnswer("error")));
+            log.error(e.getMessage(), e);
+        }
     }
 }
