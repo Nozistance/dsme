b081ffc55c13f22ff5b93bb5bf1fb2435e9df5d6
Nozistance <nozistance@gmail.com>
2024-02-21 11:12:21 +0300

    Add static method byName that consume enums value

--- src/main/java/io/nozistance/dsme/util/DayOfWeek.java
+++ src/main/java/io/nozistance/dsme/util/DayOfWeek.java
@@ -2,6 +2,11 @@ package io.nozistance.dsme.util;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
 public enum DayOfWeek {
 
@@ -13,7 +18,22 @@ public enum DayOfWeek {
     SATURDAY("Суббота"),
     SUNDAY("Воскресенье");
 
+    private static final Map<String, DayOfWeek> days;
+
+    static {
+        days = Arrays.stream(DayOfWeek.values())
+                .collect(Collectors.toMap(
+                        DayOfWeek::toString,
+                        Function.identity(),
+                        (x, y) -> y
+                ));
+    }
+
     private final String day;
+
+    public static DayOfWeek byName(String dayName) {
+        return days.get(dayName);
+    }
 
     @Override
     public String toString() {
