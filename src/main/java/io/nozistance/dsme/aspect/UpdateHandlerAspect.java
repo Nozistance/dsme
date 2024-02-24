1986e27e1cbd6f4db80f7143a676e80ae6890ec6
Nozistance <nozistance@gmail.com>
2024-02-24 18:20:40 +0300

    Rework exception handling for UpdateHandler's

--- src/main/java/io/nozistance/dsme/aspect/UpdateHandlerAspect.java
+++ src/main/java/io/nozistance/dsme/aspect/UpdateHandlerAspect.java
@@ -1,31 +1,40 @@
 package io.nozistance.dsme.aspect;
 
-import io.nozistance.dsme.service.AnswerTextService;
-import io.nozistance.dsme.telegram.UpdateAnswer;
-import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
+import io.nozistance.dsme.telegram.CallbackQueryAnswer;
+import io.nozistance.dsme.telegram.CommandAnswer;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.bots.AbsSender;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Slf4j
 @Aspect
 @Component
-@RequiredArgsConstructor
 public class UpdateHandlerAspect {
 
-    private final AnswerTextService answers;
-
-    @SneakyThrows
-    @AfterThrowing(value = "execution(void io.nozistance.dsme.telegram.UpdateHandler+.handle(..))", throwing = "e")
-    public void init(JoinPoint joinPoint, Throwable e) {
+    @Around(value = "execution(void io.nozistance.dsme.telegram.UpdateHandler+.handle(..))")
+    public Object handle(ProceedingJoinPoint joinPoint) {
         Update update = (Update) joinPoint.getArgs()[0];
         AbsSender sender = (AbsSender) joinPoint.getArgs()[1];
-        log.error(e.getMessage(), e);
-        sender.execute(new UpdateAnswer(update, answers.getAnswer("error")));
+        try {
+            return joinPoint.proceed();
+        } catch (TelegramApiException e) {
+            log.error(e.getMessage());
+            return null;
+        } catch (Throwable e) {
+            try {
+                if (update.hasCallbackQuery()) {
+                    sender.execute(new CallbackQueryAnswer(update, "SERVER INTERNAL ERROR"));
+                } else {
+                    sender.execute(new CommandAnswer(update, "SERVER INTERNAL ERROR"));
+                }
+            } catch (TelegramApiException ignored) {}
+            log.error(e.getMessage(), e);
+            return null;
+        }
     }
 }
