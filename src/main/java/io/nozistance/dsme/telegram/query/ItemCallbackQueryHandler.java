c6a7d98ccfd42148aaa0467dcbcc8801e4821d32
Nozistance <nozistance@gmail.com>
2024-02-24 23:35:18 +0300

    Refactor ItemCallbackQueryHandler.java

--- src/main/java/io/nozistance/dsme/telegram/query/ItemCallbackQueryHandler.java
+++ src/main/java/io/nozistance/dsme/telegram/query/ItemCallbackQueryHandler.java
@@ -3,6 +3,7 @@ package io.nozistance.dsme.telegram.query;
 import io.nozistance.dsme.model.Item;
 import io.nozistance.dsme.repository.MenuRepository;
 import io.nozistance.dsme.service.ImageFetchingService;
+import io.nozistance.dsme.telegram.UpdateAnswer;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Component;
@@ -28,11 +29,15 @@ public class ItemCallbackQueryHandler implements CallbackQueryHandler {
     public void answer(Update update, AbsSender sender) {
         UUID itemId = UUID.fromString(args(update));
         Optional<Item> optional = menuRepository.findById(itemId);
-        if (optional.isPresent()) {
-            Item item = optional.get();
+        if (optional.isEmpty()) return;
+        Item item = optional.get();
+        if (item.getImage().isEmpty()) {
+            sender.execute(new UpdateAnswer(update, "HELLO BLYAT"));
+        } else {
             var photo = getImage(item.getImage());
             sender.execute(SendPhoto.builder()
                     .chatId(update.getCallbackQuery().getMessage().getChatId())
+                    .caption(item.getImage())
                     .photo(photo)
                     .build());
         }
