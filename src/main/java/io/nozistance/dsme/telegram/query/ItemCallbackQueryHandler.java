9a2fb29025ef8328ce86c1df6e25b74603bd6ead
Nozistance <nozistance@gmail.com>
2024-02-24 18:17:23 +0300

    Implement 'item' callback query handing

---
+++ src/main/java/io/nozistance/dsme/telegram/query/ItemCallbackQueryHandler.java
@@ -0,0 +1,51 @@
+package io.nozistance.dsme.telegram.query;
+
+import io.nozistance.dsme.model.Item;
+import io.nozistance.dsme.repository.MenuRepository;
+import io.nozistance.dsme.service.ImageFetchingService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.bots.AbsSender;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class ItemCallbackQueryHandler implements CallbackQueryHandler {
+
+    private final MenuRepository menuRepository;
+    private final ImageFetchingService imageFetchingService;
+
+    @Override
+    @SneakyThrows
+    public void answer(Update update, AbsSender sender) {
+        UUID itemId = UUID.fromString(args(update));
+        Optional<Item> optional = menuRepository.findById(itemId);
+        if (optional.isPresent()) {
+            Item item = optional.get();
+            var photo = getImage(item.getImage());
+            sender.execute(SendPhoto.builder()
+                    .chatId(update.getCallbackQuery().getMessage().getChatId())
+                    .photo(photo)
+                    .build());
+        }
+    }
+
+    private InputFile getImage(String path) {
+        byte[] bytes = imageFetchingService.fetchFrom(path);
+        InputStream stream = new ByteArrayInputStream(bytes);
+        return new InputFile(stream, path);
+    }
+
+    @Override
+    public String getQuery() {
+        return "item";
+    }
+}
