29219c49d27498f8dddce82772310d7f3caaea9c
Nozistance <nozistance@gmail.com>
2024-02-24 18:21:45 +0300

    Add callback query auto-answering for CallbackQueryHandler's

--- src/main/java/io/nozistance/dsme/telegram/query/CallbackQueryHandler.java
+++ src/main/java/io/nozistance/dsme/telegram/query/CallbackQueryHandler.java
@@ -1,11 +1,12 @@
 package io.nozistance.dsme.telegram.query;
 
+import io.nozistance.dsme.telegram.CallbackQueryAnswer;
 import io.nozistance.dsme.telegram.UpdateHandler;
+import lombok.SneakyThrows;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.bots.AbsSender;
 
 public interface CallbackQueryHandler extends UpdateHandler {
-
-    String getQuery();
 
     @Override
     default boolean supports(Update update) {
@@ -13,6 +14,16 @@ public interface CallbackQueryHandler extends UpdateHandler {
                 && update.getCallbackQuery().getData()
                 .startsWith(getQuery() + ":");
     }
+
+    String getQuery();
+
+    @SneakyThrows
+    default void handle(Update update, AbsSender sender) {
+        sender.execute(new CallbackQueryAnswer(update));
+        answer(update, sender);
+    }
+
+    void answer(Update update, AbsSender absSender);
 
     default String args(Update update) {
         return update.getCallbackQuery()
--- src/main/java/io/nozistance/dsme/telegram/query/DayCallbackQueryHandler.java
+++ src/main/java/io/nozistance/dsme/telegram/query/DayCallbackQueryHandler.java
@@ -3,12 +3,11 @@ package io.nozistance.dsme.telegram.query;
 import io.nozistance.dsme.repository.MenuRepository;
 import io.nozistance.dsme.service.AnswerTextService;
 import io.nozistance.dsme.service.KeyboardService;
-import io.nozistance.dsme.telegram.UpdateAnswer;
+import io.nozistance.dsme.telegram.CommandAnswer;
 import io.nozistance.dsme.util.DayOfWeek;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.meta.api.methods.AnswerCallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -25,10 +24,9 @@ public class DayCallbackQueryHandler implements CallbackQueryHandler {
 
     @Override
     @SneakyThrows(TelegramApiException.class)
-    public void handle(Update update, AbsSender sender) {
+    public void answer(Update update, AbsSender sender) {
         DayOfWeek day = DayOfWeek.values()[Integer.parseInt(args(update))];
-        sender.execute(new AnswerCallbackQuery(update.getCallbackQuery().getId()));
-        sender.execute(new UpdateAnswer(update, answers.getAnswer("day-menu", day),
+        sender.execute(new CommandAnswer(update, answers.getAnswer("day-menu", day),
                 keyboards.singleColumn(menuRepository.findByDaysOfWeekContains(day).stream()
                         .map(i -> of(i.getName() + " | " + i.getPrice(), "item:" + i.getId()))
                         .toList())));
