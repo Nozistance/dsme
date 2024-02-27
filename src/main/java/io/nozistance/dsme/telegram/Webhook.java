98d124bb619db1c5a05c729b75da1c3e55be383c
Nozistance <nozistance@gmail.com>
2024-02-27 20:12:21 +0300

    Remove unused ToString annotation

--- src/main/java/io/nozistance/dsme/telegram/Webhook.java
+++ src/main/java/io/nozistance/dsme/telegram/Webhook.java
@@ -2,13 +2,11 @@ package io.nozistance.dsme.telegram;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.ToString;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Getter
-@ToString
 @RequiredArgsConstructor
 public class Webhook extends TelegramWebhookBot {
 
